731d34f04ae58a347483e4a4375c39db19c9419e
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-13 18:06:27 +0300

    add task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+function createElementsCard(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+        <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>
+</li>
+        `;
+    })
+    .join('');
+}
+
+console.log(createElementsCard(galleryItems));
+const item = document.querySelector('.gallery');
+item.style.listStyle = 'none';
+const cardsMarkup = createElementsCard(galleryItems);
+item.insertAdjacentHTML('beforeend', cardsMarkup);
+/*** */
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
