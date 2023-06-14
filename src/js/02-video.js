3cb3f183ac4c78cbd58e30ddf814438fd025b910
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-15 00:15:10 +0300

    add task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onPlay = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+let throttled = throttle(onPlay, 1000);
+//player.on('timeupdate', throttle(onPlay, 1000));
+
+player.on('timeupdate', throttled);
+
+const getTime = localStorage.getItem('videoplayer-current-time');
+
+if (!localStorage.getItem('videoplayer-current-time')) {
+  return null;
+}
+console.log(getTime);
+
+player
+  .setCurrentTime(getTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
